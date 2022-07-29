89a26ae703ad78b57f342167bf60b03b53278662
beambitioustiger@gmail.com <beambitioustiger@gmail.com>
2022-07-29 15:25:13 +0900

    recipesテーブルのモデルクラスを書いた

--- controller/recipesControlFunc.js
+++ controller/recipesControlFunc.js
@@ -2,7 +2,20 @@ const recipesModelHandler = require('../model/recipesHandler');
 
 const recipesController = {
     
+    // recipes/today
     async today(req, res) {
+        // ログインチェック
+        
+        // sessionからusers_idをもってくる
+        const users_id = "0";
+
+        const recipes_db = new recipesModelHandler;
+        const result = await recipes_db.getForToday(users_id);
+
+        const data = {
+            "contents":result
+        }
+        /*
         const data = {
             "contents":[
                 {
@@ -16,10 +29,24 @@ const recipesController = {
                     "resorce":"もも肉<br>トマト缶<br>塩コショウ<br>トマト<br>"
                 }
         ]}
+        */
         res.render('recipes/today', data);
     },
 
+    // recipes/all
     async all(req, res) {
+        // ログインチェック
+        
+        // sessionからusers_idをもってくる
+        const users_id = "0";
+
+        const recipes_db = new recipesModelHandler;
+        const result = await recipes_db.getAll(users_id);
+
+        const data = {
+            "contents":result
+        }
+        /*
         const data = {
             "contents":[
                 {
@@ -31,11 +58,25 @@ const recipesController = {
                     "name":"鶏肉のトマト煮"
                 }
         ]}
+        */
         res.render('recipes/all', data);
     },
     
+    // recipes/recipe/:recipes_id
     async recipe(req, res) {
+        // ログインチェック
+        
+        // URLからrecipes_idをもってくる
+        const recipes_id = "0";
 
+        const recipes_db = new recipesModelHandler;
+        const result = await recipes_db.getRecipe(recipes_id);
+
+        // \n -> <br>の処理
+
+
+        const data = result;
+        /*
         // \n -> <br>
         const data = {
             "recipes_id":1,
@@ -43,19 +84,51 @@ const recipesController = {
             "resorce":"もも肉<br>ニンニク<br>片栗粉<br>塩コショウ<br>オイル",
             "way":"1. もも肉にした味をつけ片栗粉をまぶす<br>2. フライパンにオイルとニンニクを入れ加熱する<br>3. 1のもも肉をフライパンに入れ加熱する<br>4. 両面を加熱して出来上がり"
         }
+        */
         res.render('recipes/recipe', data);
     },
     
+    // recipes/add
     async addGet(req, res) {
+        // ログインチェック
+        
         res.render('recipes/add');
     },
     
+    // recipes/add
     async addPost(req, res) {
+        // ログインチェック
+        
+        // sessionからusers_idをもってくる
+        const users_id = "0";
+
+        // フォームから入力データをもってくる
+        const name = "test";
+        const resorce = "test";
+        const way = "test";
+
+        const recipes_db = new recipesModelHandler;
+        const result = await recipes_db.addRecipe(users_id, name, resorce, way);
+
         res.redirect('/recipes/all');
     },
 
+    // recipes/edit:recipes_id
     async editGet(req, res) {
+        // ログインチェック
+        
+        // URLからrecipes_idをもってくる
+        const recipes_id = "0";
 
+        const recipes_db = new recipesModelHandler;
+        const result = await recipes_db.getRecipe(recipes_id);
+
+        // <br> -> \nの処理
+
+        
+        const data = result;
+
+        /*
         // <br> -> \n
         const data = {
             "recipes_id":1,
@@ -63,10 +136,25 @@ const recipesController = {
             "resorce":"もも肉\nニンニク\n片栗粉\n塩コショウ\nオイル",
             "way":"1. もも肉にした味をつけ片栗粉をまぶす\n2. フライパンにオイルとニンニクを入れ加熱する\n3. 1のもも肉をフライパンに入れ加熱する\n4. 両面を加熱して出来上がり"
         }
+        */
         res.render('recipes/edit', data);
     },
 
+    // recipes/edit
     async editPost(req, res) {
+        // ログインチェック
+        
+        // URLからrecipes_idをもってくる
+        const recipes_id = "0";
+
+        // フォームから入力データをもってくる
+        const name = "test";
+        const resorce = "test";
+        const way = "test";
+
+        const recipes_db = new recipesModelHandler;
+        const result = await recipes_db.addRecipe(users_id, name, resorce, way);
+
         res.redirect('/recipes/recipe/:recipes_id');
     }
 
--- model/recipesHandler.js
+++ model/recipesHandler.js
@@ -1,7 +1,137 @@
 const dbHandleFunc = require('./dbHandleFunc');
 
 class recipesHandler {
+    /* 
+        クラス概要
+            データベースのテーブルであるrecipesへの操作のメソッドをまとめたクラス
+        
+        クラスメソッド
+            getForToday
+            getAll
+            getRecipe
+            addRecipe
+            editRecipe
+    */
+    
+    /*
+        メモ
+            ・userの持ってるレシピの一覧を持ってくる
+            ・リストをランダムに、履歴分を排除の機能をつける
+        
+        概要
+            与えられたusers_idに該当する全てのレコードのrecepes_id, name, resorceを返す
 
+        呼び出し
+            recipesControllerFunc.today
+
+        引数
+            users_id
+
+        返り値
+            [{"recipes_id":recipes_id, "name":name, "resorce":resorce}, ...]
+    */
+    async getForToday(users_id) {
+        const handle_func = new dbHandleFunc;
+        const query = "SELECT recipes_id, name, resorce from recipes where users_id = ?";
+        const values = [users_id];
+        
+        const result = await handle_func.executeQuery(query, values);
+
+        return result;
+    }
+
+    /*
+        概要
+            与えられたusers_idに該当する全てのレコードのrecepes_id, nameを返す
+
+        呼び出し
+            recipesControllerFunc.all
+
+        引数
+            users_id
+
+        返り値
+            [{"recipes_id":recipes_id, "name":name}, ...]
+    */
+    async getAll(users_id) {
+        const handle_func = new dbHandleFunc;
+        const query = "SELECT recipes_id, name from recipes where users_id = ?";
+        const values = [users_id];
+        
+        const result = await handle_func.executeQuery(query, values);
+
+        return result;
+    }
+
+    /*
+        概要
+            与えられたrecipes_idに該当するレコードを返す
+
+        呼び出し
+            recipesControllerFunc.recipe
+            recipesControllerFunc.edit
+
+        引数
+            recipes_id
+
+        返り値
+            {"recipes_id":recipes, "name":name, "resorce":resorce, "way":way}
+    */
+    async getRecipe(recipes_id) {
+        const handle_func = new dbHandleFunc;
+        const query = "SELECT recipes_id, name, resorce, way from recipes where recipes_id = ?";
+        const values = [recipes_id];
+                
+        const result = await handle_func.executeQuery(query, values);
+    
+        return result;
+    }
+
+    /*
+        概要
+            与えられた入力データで新しいレコードをrecipesに挿入する
+
+        呼び出し
+            recipesControllerFunc.addPost
+
+        引数
+            users_id, name, resorce, way
+
+        返り値
+            なし
+    */
+    async addRecipe(users_id, name, resorce, way) {
+        const handle_func = new dbHandleFunc;
+        const query = "INSERT INTO recipes (users_id, name, resorce, way) VALUES (?, ?, ?, ?)";
+        const values = [users_id, name, resorce, way];
+                        
+        const result = await handle_func.executeQuery(query, values);
+            
+        return ;
+    }
+
+    /*
+        概要
+            対象のレコードのデータを与えられた入力データに更新する
+
+        呼び出し
+            recipesControllerFunc.editPost
+
+        引数
+            users_id, name, resorce, way
+
+        返り値
+            なし
+    */
+    async editRecipe(recipes_id, name, resorce, way) {
+        const handle_func = new dbHandleFunc;
+        const query = "UPDATE recipes SET name = ?, resorce = ?, way = ? WHERE recipes_id = ?";
+        const values = [name, resorce, way, recipes_id];
+                                
+        const result = await handle_func.executeQuery(query, values);
+                    
+        return ;
+    }
 }
 
 module.exports = recipesHandler;
