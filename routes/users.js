6e9c9ff82e652c596fe50bfed5c5cca09e8cea1f
beambitioustiger@gmail.com <beambitioustiger@gmail.com>
2022-09-07 21:25:58 +0900

    9_7 調整

--- routes/users.js
+++ routes/users.js
@@ -8,12 +8,7 @@ const usersController = require('../controller/usersControlFunc');
 /*** users/login ******************************/
 router.get('/login', csrfProtection, usersController.loginGet);
 router.post('/login', csrfProtection, usersController.loginPost);
-/*
-router.post('/login', passport.authenticate('local', 
-{successRedirect: '/recipes/today',
-failureRedirect: '/login',
-session: true}), usersController.loginPost);
-*/
+
 /*** users/add ******************************/
 router.get('/add', csrfProtection, usersController.addGet);
 router.post('/add', usersController.addPost);
